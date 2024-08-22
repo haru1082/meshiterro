1a12ca58dd6fe09ff60da93d4a468deb7daa472d
EC2 Default User <ec2-user@ip-172-31-19-10.ap-northeast-1.compute.internal>
2024-08-22 07:31:27 +0000

    ページネーション実装完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 
-devise_for :users
-root to: "homes#top"
+ devise_for :users
+ root to: "homes#top"
+ get "/about" => "homes#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-resources :post_images, only: [:new, :create, :index, :show, :destroy] do
- resources :post_comments, only: [:create, :destroy]
-end 
-resources :users, only: [:show, :edit, :update]
-
-get "/about" => "homes#about"
+ resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+  resource :favorite, only: [:create, :destroy]
+  resources :post_comments, only: [:create, :destroy]
+ end 
+ resources :users, only: [:show, :edit, :update]
 
 end
